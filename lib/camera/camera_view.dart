abdc23bf10fa4bdf5089cddbc1a2234a1bf7acf2
Francis James Tolentino <francistolentino1107@gmail.com>
2022-07-29 15:08:05 +0800

    ui

--- lib/camera/camera_view.dart
+++ lib/camera/camera_view.dart
@@ -79,26 +79,26 @@ class _CameraViewState extends State<CameraView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          if (_allowPicker)
-            Padding(
-              padding: const EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                onTap: _switchScreenMode,
-                child: Icon(
-                  _mode == ScreenMode.liveFeed
-                      ? Icons.photo_library_outlined
-                      : (Platform.isIOS
-                          ? Icons.camera_alt_outlined
-                          : Icons.camera),
-                ),
-              ),
-            ),
-        ],
-      ),
-      body: _body(),
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      //   actions: [
+      //     if (_allowPicker)
+      //       Padding(
+      //         padding: const EdgeInsets.only(right: 20.0),
+      //         child: GestureDetector(
+      //           onTap: _switchScreenMode,
+      //           child: Icon(
+      //             _mode == ScreenMode.liveFeed
+      //                 ? Icons.photo_library_outlined
+      //                 : (Platform.isIOS
+      //                     ? Icons.camera_alt_outlined
+      //                     : Icons.camera),
+      //           ),
+      //         ),
+      //       ),
+      //   ],
+      // ),
+      body: _liveFeedBody(),
       floatingActionButton: _floatingActionButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
@@ -108,28 +108,41 @@ class _CameraViewState extends State<CameraView> {
     if (_mode == ScreenMode.gallery) return null;
     if (cameras.length == 1) return null;
     return SizedBox(
-        height: 70.0,
-        width: 70.0,
-        child: FloatingActionButton(
-          onPressed: _switchLiveCamera,
-          child: Icon(
-            Platform.isIOS
-                ? Icons.flip_camera_ios_outlined
-                : Icons.flip_camera_android_outlined,
-            size: 40,
+      height: 80.0,
+      width: 80.0,
+      child: GestureDetector(
+        onTap: _switchLiveCamera,
+        child: CircleAvatar(
+          backgroundColor: Colors.white,
+          child: CircleAvatar(
+            radius: 38,
+            backgroundColor: Colors.black87,
+            child: CircleAvatar(
+              radius: 34,
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black87,
+              child: Icon(
+                Platform.isIOS
+                    ? Icons.flip_camera_ios_outlined
+                    : Icons.flip_camera_android_outlined,
+                size: 40,
+              ),
+            ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 
-  Widget _body() {
-    Widget body;
-    if (_mode == ScreenMode.liveFeed) {
-      body = _liveFeedBody();
-    } else {
-      body = _galleryBody();
-    }
-    return body;
-  }
+  // Widget _body() {
+  //   Widget body;
+  //   if (_mode == ScreenMode.liveFeed) {
+  //     body = _liveFeedBody();
+  //   } else {
+  //     body = _galleryBody();
+  //   }
+  //   return body;
+  // }
 
   Widget _liveFeedBody() {
     if (_controller?.value.isInitialized == false) {
@@ -137,13 +150,8 @@ class _CameraViewState extends State<CameraView> {
     }
 
     final size = MediaQuery.of(context).size;
-    // calculate scale depending on screen and camera ratios
-    // this is actually size.aspectRatio / (1 / camera.aspectRatio)
-    // because camera preview size is received as landscape
-    // but we're calculating for portrait orientation
     var scale = size.aspectRatio * _controller!.value.aspectRatio;
 
-    // to prevent scaling down, invert the value
     if (scale < 1) scale = 1 / scale;
 
     return Container(
@@ -163,7 +171,7 @@ class _CameraViewState extends State<CameraView> {
           ),
           if (widget.customPaint != null) widget.customPaint!,
           Positioned(
-            bottom: 100,
+            bottom: 170,
             left: 50,
             right: 50,
             child: Slider(
@@ -180,86 +188,85 @@ class _CameraViewState extends State<CameraView> {
                   ? null
                   : (maxZoomLevel - 1).toInt(),
             ),
-          )
+          ),
         ],
       ),
     );
   }
 
-  Widget _galleryBody() {
-    return ListView(shrinkWrap: true, children: [
-      _image != null
-          ? SizedBox(
-              height: 400,
-              width: 400,
-              child: Stack(
-                fit: StackFit.expand,
-                children: <Widget>[
-                  Image.file(_image!),
-                  if (widget.customPaint != null) widget.customPaint!,
-                ],
-              ),
-            )
-          : const Icon(
-              Icons.image,
-              size: 200,
-            ),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ElevatedButton(
-          child: const Text('From Gallery'),
-          onPressed: () => _getImage(ImageSource.gallery),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ElevatedButton(
-          child: const Text('Take a picture'),
-          onPressed: () => _getImage(ImageSource.camera),
-        ),
-      ),
-      if (_image != null)
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-              '${_path == null ? '' : 'Image path: $_path'}\n\n${widget.text ?? ''}'),
-        ),
-    ]);
-  }
+  // Widget _galleryBody() {
+  //   return ListView(shrinkWrap: true, children: [
+  //     _image != null
+  //         ? SizedBox(
+  //             height: 400,
+  //             width: 400,
+  //             child: Stack(
+  //               fit: StackFit.expand,
+  //               children: <Widget>[
+  //                 Image.file(_image!),
+  //                 if (widget.customPaint != null) widget.customPaint!,
+  //               ],
+  //             ),
+  //           )
+  //         : const Icon(
+  //             Icons.image,
+  //             size: 200,
+  //           ),
+  //     Padding(
+  //       padding: const EdgeInsets.symmetric(horizontal: 16),
+  //       child: ElevatedButton(
+  //         child: const Text('From Gallery'),
+  //         onPressed: () => _getImage(ImageSource.gallery),
+  //       ),
+  //     ),
+  //     Padding(
+  //       padding: const EdgeInsets.symmetric(horizontal: 16),
+  //       child: ElevatedButton(
+  //         child: const Text('Take a picture'),
+  //         onPressed: () => _getImage(ImageSource.camera),
+  //       ),
+  //     ),
+  //     if (_image != null)
+  //       Padding(
+  //         padding: const EdgeInsets.all(16.0),
+  //         child: Text(
+  //             '${_path == null ? '' : 'Image path: $_path'}\n\n${widget.text ?? ''}'),
+  //       ),
+  //   ]);
+  // }
 
-  Future _getImage(ImageSource source) async {
-    setState(() {
-      _image = null;
-      _path = null;
-    });
-    final pickedFile = await _imagePicker?.pickImage(source: source);
-    if (pickedFile != null) {
-      _processPickedFile(pickedFile);
-    }
-    setState(() {});
-  }
+  // Future _getImage(ImageSource source) async {
+  //   setState(() {
+  //     _image = null;
+  //     _path = null;
+  //   });
+  //   final pickedFile = await _imagePicker?.pickImage(source: source);
+  //   if (pickedFile != null) {
+  //     _processPickedFile(pickedFile);
+  //   }
+  //   setState(() {});
+  // }
 
-  void _switchScreenMode() {
-    _image = null;
-    if (_mode == ScreenMode.liveFeed) {
-      _mode = ScreenMode.gallery;
-      _stopLiveFeed();
-    } else {
-      _mode = ScreenMode.liveFeed;
-      _startLiveFeed();
-    }
-    if (widget.onScreenModeChanged != null) {
-      widget.onScreenModeChanged!(_mode);
-    }
-    setState(() {});
-  }
+  // void _switchScreenMode() {
+  //   _image = null;
+  //   if (_mode == ScreenMode.liveFeed) {
+  //     _mode = ScreenMode.gallery;
+  //     _stopLiveFeed();
+  //   } else {
+  //     _mode = ScreenMode.liveFeed;
+  //     _startLiveFeed();
+  //   }
+  //   if (widget.onScreenModeChanged != null) {
+  //     widget.onScreenModeChanged!(_mode);
+  //   }
+  //   setState(() {});
+  // }
 
   Future _startLiveFeed() async {
     final camera = cameras[_cameraIndex];
     _controller = CameraController(
       camera,
-      ResolutionPreset.high,
-      enableAudio: false,
+      ResolutionPreset.max,
     );
     _controller?.initialize().then((_) {
       if (!mounted) {
@@ -272,7 +279,7 @@ class _CameraViewState extends State<CameraView> {
       _controller?.getMaxZoomLevel().then((value) {
         maxZoomLevel = value;
       });
-      _controller?.startImageStream(_processCameraImage);
+      // _controller?.startImageStream(_processCameraImage);
       setState(() {});
     });
   }
@@ -292,58 +299,58 @@ class _CameraViewState extends State<CameraView> {
     setState(() => _changingCameraLens = false);
   }
 
-  Future _processPickedFile(XFile? pickedFile) async {
-    final path = pickedFile?.path;
-    if (path == null) {
-      return;
-    }
-    setState(() {
-      _image = File(path);
-    });
-    _path = path;
-    final inputImage = InputImage.fromFilePath(path);
-    widget.onImage(inputImage);
-  }
+  // Future _processPickedFile(XFile? pickedFile) async {
+  //   final path = pickedFile?.path;
+  //   if (path == null) {
+  //     return;
+  //   }
+  //   setState(() {
+  //     _image = File(path);
+  //   });
+  //   _path = path;
+  //   final inputImage = InputImage.fromFilePath(path);
+  //   widget.onImage(inputImage);
+  // }
 
-  Future _processCameraImage(CameraImage image) async {
-    final WriteBuffer allBytes = WriteBuffer();
-    for (final Plane plane in image.planes) {
-      allBytes.putUint8List(plane.bytes);
-    }
-    final bytes = allBytes.done().buffer.asUint8List();
+  // Future _processCameraImage(CameraImage image) async {
+  //   final WriteBuffer allBytes = WriteBuffer();
+  //   for (final Plane plane in image.planes) {
+  //     allBytes.putUint8List(plane.bytes);
+  //   }
+  //   final bytes = allBytes.done().buffer.asUint8List();
 
-    final Size imageSize =
-        Size(image.width.toDouble(), image.height.toDouble());
+  //   final Size imageSize =
+  //       Size(image.width.toDouble(), image.height.toDouble());
 
-    final camera = cameras[_cameraIndex];
-    final imageRotation =
-        InputImageRotationValue.fromRawValue(camera.sensorOrientation);
-    if (imageRotation == null) return;
+  //   final camera = cameras[_cameraIndex];
+  //   final imageRotation =
+  //       InputImageRotationValue.fromRawValue(camera.sensorOrientation);
+  //   if (imageRotation == null) return;
 
-    final inputImageFormat =
-        InputImageFormatValue.fromRawValue(image.format.raw);
-    if (inputImageFormat == null) return;
+  //   final inputImageFormat =
+  //       InputImageFormatValue.fromRawValue(image.format.raw);
+  //   if (inputImageFormat == null) return;
 
-    final planeData = image.planes.map(
-      (Plane plane) {
-        return InputImagePlaneMetadata(
-          bytesPerRow: plane.bytesPerRow,
-          height: plane.height,
-          width: plane.width,
-        );
-      },
-    ).toList();
+  //   final planeData = image.planes.map(
+  //     (Plane plane) {
+  //       return InputImagePlaneMetadata(
+  //         bytesPerRow: plane.bytesPerRow,
+  //         height: plane.height,
+  //         width: plane.width,
+  //       );
+  //     },
+  //   ).toList();
 
-    final inputImageData = InputImageData(
-      size: imageSize,
-      imageRotation: imageRotation,
-      inputImageFormat: inputImageFormat,
-      planeData: planeData,
-    );
+  //   final inputImageData = InputImageData(
+  //     size: imageSize,
+  //     imageRotation: imageRotation,
+  //     inputImageFormat: inputImageFormat,
+  //     planeData: planeData,
+  //   );
 
-    final inputImage =
-        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+  //   final inputImage =
+  //       InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
 
-    widget.onImage(inputImage);
-  }
+  //   widget.onImage(inputImage);
+  // }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:android_image_processing/screens/object_detection.dart';
+import 'package:android_image_processing/camera/camera_view.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 List<CameraDescription> cameras = [];
+
+enum PainterFeature { ObjectDetection, ColorRecognition, TextRecognition }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,29 +39,199 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  CustomPaint? _customPaint;
+  PainterFeature _painterFeature = PainterFeature.ColorRecognition;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _setPainterFeature(PainterFeature feature) {
+    setState(() {
+      _painterFeature = feature;
+    });
+  }
+
+  CustomPaint? _painter() {
+    if (_painterFeature == PainterFeature.ObjectDetection) {
+      // _painterFeature = // whatever it is.
+    }
+    if (_painterFeature == PainterFeature.ColorRecognition) {
+      // _painterFeature = // whatever it is.
+    }
+    if (_painterFeature == PainterFeature.TextRecognition) {
+      // _painterFeature = // whatever it is.
+    }
+    return _customPaint;
+  }
+
+  Widget _header() {
+    late String text;
+
+    if (_painterFeature == PainterFeature.ObjectDetection) {
+      text = 'Object Recognition';
+    }
+    if (_painterFeature == PainterFeature.ColorRecognition) {
+      text = 'Color Recognition';
+    }
+    if (_painterFeature == PainterFeature.TextRecognition) {
+      text = 'Text Recognition';
+    }
+
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.09,
+      color: Colors.transparent,
+      child: Positioned(
+        top: 0,
+        right: 0,
+        left: 0,
+        child: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: Icon(
+                  Icons.more_vert_outlined,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Stack(
+        children: <Widget>[
+          CameraView(
+            title: 'title',
+            customPaint: _painter(),
+            onImage: ((inputImage) {}),
+          ),
+          _header(),
+          PainterController(
+            painterFeature: _painterFeature,
+            setPainterFeature: _setPainterFeature,
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: ((context) => const ObjectDetectionScreen()),
-                  ),
-                );
-              },
-              child: const Text('Object Detection'),
-            ),
-          ],
+    );
+  }
+}
+
+class PainterController extends StatefulWidget {
+  const PainterController({
+    Key? key,
+    required this.setPainterFeature,
+    required this.painterFeature,
+  }) : super(key: key);
+
+  final void Function(PainterFeature) setPainterFeature;
+  final PainterFeature painterFeature;
+
+  @override
+  State<PainterController> createState() => PainterControllerState();
+}
+
+class PainterControllerState extends State<PainterController> {
+  Widget _container(String text, PainterFeature feature) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: widget.painterFeature == feature
+            ? [
+                const BoxShadow(
+                  color: Color.fromARGB(255, 73, 73, 73),
+                  blurRadius: 7,
+                  spreadRadius: 2,
+                  offset: Offset(0.0, 5.0),
+                ),
+              ]
+            : [
+                const BoxShadow(color: Colors.transparent),
+              ],
+        color: widget.painterFeature == feature
+            ? Colors.white
+            : Colors.transparent,
+        border: Border.all(color: Colors.white, width: 0.3),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 12,
+          color: widget.painterFeature == feature ? Colors.pink : Colors.white,
         ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 110,
+      right: 0,
+      left: 0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: GestureDetector(
+              onTap: () =>
+                  widget.setPainterFeature(PainterFeature.ObjectDetection),
+              child: _container(
+                  'Object Recognition', PainterFeature.ObjectDetection),
+            ),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () =>
+                  widget.setPainterFeature(PainterFeature.ColorRecognition),
+              child: _container(
+                  'Color Recognition', PainterFeature.ColorRecognition),
+            ),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () =>
+                  widget.setPainterFeature(PainterFeature.TextRecognition),
+              child: _container(
+                  'Text Recognition', PainterFeature.TextRecognition),
+            ),
+          ),
+        ],
       ),
     );
   }
