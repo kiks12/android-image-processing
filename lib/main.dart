575843c47852aea56c52267e4f45213be69cacf1
Francis James Tolentino <francistolentino1107@gmail.com>
2022-08-03 00:11:28 +0800

    not fucking working

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
+import 'dart:math' as math;
 
 import 'package:android_image_processing/camera/camera_view.dart';
 import 'package:android_image_processing/painters/object_detector_painter.dart';
 import 'package:android_image_processing/painters/rectangle_painter.dart';
+import 'package:android_image_processing/widgets/main_header.dart';
 import 'package:android_image_processing/widgets/painter_controller.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
@@ -10,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:palette_generator/palette_generator.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 
@@ -89,6 +92,13 @@ class _HomeScreenState extends State<HomeScreen> {
 
   /* Camera Controller Variables */
   late CameraController _cameraController;
+  /*  */
+
+  /* Palette Generator Variables */
+  Rect? region;
+  PaletteGenerator? paletteGenerator;
+  String text = '';
+  /*  */
 
   @override
   void initState() {
@@ -108,6 +118,23 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
+  /* PALETTE GENERATOR CODES */
+  Future<void> _updatePaletteGenerator(Rect? newRegion, dynamic image) async {
+    PaletteGenerator.fromImage(
+      image,
+      region: newRegion!,
+      maximumColorCount: 20,
+    );
+    // paletteGenerator = await PaletteGenerator.fromImage(
+    //   image,
+    //   region: newRegion!,
+    //   maximumColorCount: 20,
+    // );
+    setState(() {});
+  }
+  /* PALETTE GENERATOR CODES */
+
+  /* TEXT TO SPEECH FUNCTIONS */
   Future _setAwaitOptions() async {
     await flutterTts.awaitSpeakCompletion(true);
   }
@@ -128,13 +155,61 @@ class _HomeScreenState extends State<HomeScreen> {
       _getDefaultEngine();
     }
   }
+  /* TEXT TO SPEECH FUNCTIONS */
 
+  /* PAINTER MENU CONTROLLER FUNCTIONS */
   void _setPainterFeature(PainterFeature feature) {
+    xPoint = null;
+    yPoint = null;
+    x = null;
+    y = null;
+    w = null;
+    h = null;
+    _customPaint = null;
     _painterFeature = feature;
     if (feature == PainterFeature.ObjectDetection) {
       _initializeDetector(DetectionMode.stream);
     }
     if (feature == PainterFeature.ColorRecognition) {
+      _cameraController.startImageStream((image) {
+        final WriteBuffer allBytes = WriteBuffer();
+        for (final Plane plane in image.planes) {
+          allBytes.putUint8List(plane.bytes);
+        }
+        final bytes = allBytes.done().buffer.asUint8List();
+
+        final Size imageSize =
+            Size(image.width.toDouble(), image.height.toDouble());
+
+        final inputImageFormat =
+            InputImageFormatValue.fromRawValue(image.format.raw);
+        if (inputImageFormat == null) return;
+
+        final planeData = image.planes.map(
+          (Plane plane) {
+            return InputImagePlaneMetadata(
+              bytesPerRow: plane.bytesPerRow,
+              height: plane.height,
+              width: plane.width,
+            );
+          },
+        ).toList();
+
+        final inputImageData = InputImageData(
+          size: imageSize,
+          imageRotation: InputImageRotation.rotation0deg,
+          inputImageFormat: inputImageFormat,
+          planeData: planeData,
+        );
+
+        final inputImage =
+            InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+
+        Rect newRegion = Rect.fromPoints(Offset(x!, y!), Offset(w!, h!));
+        _updatePaletteGenerator(newRegion, File(inputImage.filePath!));
+        text = inputImage.filePath!;
+        setState(() {});
+      });
       _customPaint2 = null;
     }
     if (feature == PainterFeature.TextRecognition) {
@@ -175,72 +250,26 @@ class _HomeScreenState extends State<HomeScreen> {
           h: h,
         ),
       );
+      // can add another canvas using customPainter2
     }
+
     if (_painterFeature == PainterFeature.ColorRecognition) {
-      _customPaint = const CustomPaint();
-      // _painterFeature = // whatever it is.
+      _customPaint = CustomPaint(
+        painter: RectanglePainter(
+          xPoint: xPoint,
+          yPoint: yPoint,
+          x: x,
+          y: y,
+          w: w,
+          h: h,
+        ),
+      );
+      // can add another canvas using customPainter2
     }
+
     return _customPaint;
   }
-
-  Widget _header() {
-    late String text;
-
-    if (_painterFeature == PainterFeature.ObjectDetection) {
-      text = 'Object Recognition';
-    }
-    if (_painterFeature == PainterFeature.ColorRecognition) {
-      text = 'Color Recognition';
-    }
-    if (_painterFeature == PainterFeature.TextRecognition) {
-      text = 'Text Recognition';
-    }
-
-    return Positioned(
-      top: 0,
-      right: 0,
-      left: 0,
-      child: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.06,
-          color: Colors.transparent,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Icon(
-                  Icons.arrow_back_ios,
-                  color: Colors.white,
-                ),
-              ),
-              Expanded(
-                child: Text(
-                  text,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(right: 20),
-                child: Icon(
-                  Icons.more_vert_outlined,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  /* PAINTER MENU CONTROLLER FUNCTIONS */
 
   @override
   Widget build(BuildContext context) {
@@ -249,19 +278,52 @@ class _HomeScreenState extends State<HomeScreen> {
         children: <Widget>[
           GestureDetector(
             onTapDown: _onScreenClick,
-            child: CameraView(
-              controller: _cameraController,
-              customPaint: _painter(),
-              customPaint2: _customPaint2,
-              painterFeature: _painterFeature,
-              onImage: ((inputImage) {
-                if (_painterFeature == PainterFeature.ObjectDetection) {
-                  objectDetectionProcessImage(inputImage);
-                }
-              }),
+            child: Stack(
+              children: [
+                CameraView(
+                  controller: _cameraController,
+                  customPaint: _painter(),
+                  customPaint2: _customPaint2,
+                  painterFeature: _painterFeature,
+                  onImage: ((inputImage) async {
+                    if (_painterFeature == PainterFeature.ObjectDetection) {
+                      objectDetectionProcessImage(inputImage);
+                    }
+                    if (_painterFeature == PainterFeature.ColorRecognition) {
+                      Image image = Image.file(
+                        File(inputImage.filePath!),
+                      );
+                      Rect newRegion =
+                          Rect.fromPoints(Offset(x!, y!), Offset(w!, h!));
+                      await _updatePaletteGenerator(
+                        newRegion,
+                        image,
+                      );
+                      region = newRegion;
+                      text = inputImage.filePath!;
+                      setState(() {});
+                    }
+                  }),
+                ),
+              ],
             ),
           ),
-          _header(),
+          MainHeader(painterFeature: _painterFeature),
+          if (paletteGenerator != null)
+            Positioned(
+              bottom: 300,
+              left: 50,
+              right: 50,
+              child: Center(child: Text(text)),
+            ),
+          // Positioned(
+          //   bottom: 300,
+          //   left: 40,
+          //   right: 40,
+          //   child: PaletteSwatches(
+          //     generator: paletteGenerator,
+          //   ),
+          // ),
           PainterController(
             painterFeature: _painterFeature,
             setPainterFeature: _setPainterFeature,
@@ -270,6 +332,8 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  /* OBJECT DETECTOR FUNCTIONS */
 
   Future<void> objectDetectionProcessImage(InputImage inputImage) async {
     if (!_canProcess) return;
@@ -323,5 +387,129 @@ class _HomeScreenState extends State<HomeScreen> {
           .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
     }
     return file.path;
+  }
+  /* OBJECT DETECTOR FUNCTIONS */
+
+}
+
+// A widget that draws the swatches for the [PaletteGenerator] it is given,
+/// and shows the selected target colors.
+class PaletteSwatches extends StatelessWidget {
+  /// Create a Palette swatch.
+  ///
+  // / The [generator] is optional. If it is null, then the display will
+  /// just be an empty container.
+  const PaletteSwatches({Key? key, required this.generator}) : super(key: key);
+
+  /// The [PaletteGenerator] that contains all of the swatches that we're going
+  /// to display.
+  final PaletteGenerator? generator;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> swatches = <Widget>[];
+    final PaletteGenerator? paletteGen = generator;
+    if (paletteGen == null || paletteGen.colors.isEmpty) {
+      return Container();
+    }
+    for (final Color color in paletteGen.colors) {
+      swatches.add(PaletteSwatch(color: color));
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Wrap(
+          children: swatches,
+        ),
+        Container(height: 30.0),
+        PaletteSwatch(
+            label: 'Dominant', color: paletteGen.dominantColor?.color),
+        PaletteSwatch(
+            label: 'Light Vibrant', color: paletteGen.lightVibrantColor?.color),
+        PaletteSwatch(label: 'Vibrant', color: paletteGen.vibrantColor?.color),
+        PaletteSwatch(
+            label: 'Dark Vibrant', color: paletteGen.darkVibrantColor?.color),
+        PaletteSwatch(
+            label: 'Light Muted', color: paletteGen.lightMutedColor?.color),
+        PaletteSwatch(label: 'Muted', color: paletteGen.mutedColor?.color),
+        PaletteSwatch(
+            label: 'Dark Muted', color: paletteGen.darkMutedColor?.color),
+      ],
+    );
+  }
+}
+
+/// A small square of color with an optional label.
+@immutable
+class PaletteSwatch extends StatelessWidget {
+  /// Creates a PaletteSwatch.
+  ///
+  /// If the [paletteColor] has property `isTargetColorFound` as `false`,
+  /// then the swatch will show a placeholder instead, to indicate
+  /// that there is no color.
+  const PaletteSwatch({
+    Key? key,
+    this.color,
+    this.label,
+  }) : super(key: key);
+
+  /// The color of the swatch.
+  final Color? color;
+
+  /// The optional label to display next to the swatch.
+  final String? label;
+
+  @override
+  Widget build(BuildContext context) {
+    // Compute the "distance" of the color swatch and the background color
+    // so that we can put a border around those color swatches that are too
+    // close to the background's saturation and lightness. We ignore hue for
+    // the comparison.
+    final HSLColor hslColor = HSLColor.fromColor(color ?? Colors.transparent);
+    final HSLColor backgroundAsHsl = HSLColor.fromColor(Colors.white);
+    final double colorDistance = math.sqrt(
+        math.pow(hslColor.saturation - backgroundAsHsl.saturation, 2.0) +
+            math.pow(hslColor.lightness - backgroundAsHsl.lightness, 2.0));
+
+    Widget swatch = Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: color == null
+          ? const Placeholder(
+              fallbackWidth: 34.0,
+              fallbackHeight: 20.0,
+              color: Color(0xff404040),
+              strokeWidth: 2.0,
+            )
+          : Container(
+              decoration: BoxDecoration(
+                  color: color,
+                  border: Border.all(
+                    width: 1.0,
+                    color: Colors.black,
+                    style: colorDistance < 0.2
+                        ? BorderStyle.solid
+                        : BorderStyle.none,
+                  )),
+              width: 34.0,
+              height: 20.0,
+            ),
+    );
+
+    if (label != null) {
+      swatch = ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 130.0, minWidth: 130.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            swatch,
+            Container(width: 5.0),
+            Text(label!),
+          ],
+        ),
+      );
+    }
+    return swatch;
   }
 }
---
+++ lib/widgets/main_header.dart
@@ -0,0 +1,83 @@
+import 'package:android_image_processing/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainHeader extends StatefulWidget {
+  const MainHeader({
+    Key? key,
+    required this.painterFeature,
+  }) : super(key: key);
+
+  final PainterFeature painterFeature;
+
+  @override
+  State<MainHeader> createState() => _MainHeaderState();
+}
+
+class _MainHeaderState extends State<MainHeader> {
+  late String text;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.painterFeature == PainterFeature.ObjectDetection) {
+      text = 'Object Recognition';
+    }
+    if (widget.painterFeature == PainterFeature.ColorRecognition) {
+      text = 'Color Recognition';
+    }
+    if (widget.painterFeature == PainterFeature.TextRecognition) {
+      text = 'Text Recognition';
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 0,
+      right: 0,
+      left: 0,
+      child: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.06,
+          color: Colors.transparent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: Icon(
+                  Icons.more_vert_outlined,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
